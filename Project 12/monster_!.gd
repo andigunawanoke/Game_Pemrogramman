8af034f8379563408ceb3701fd607e6633f1a73f
andigunawanoke <57738800+andigunawanoke@users.noreply.github.com>
2021-10-26 10:44:04 +0700

    Add files via upload
    Ini file versi yang udah ada platform tapi ada bug di animasinya jadi gw push pake folder baru.

---
+++ Project 12/monster_!.gd
@@ -0,0 +1,46 @@
+extends KinematicBody2D
+
+var speed = 100
+var arah = -1
+var rng = RandomNumberGenerator.new()
+var _timer = null
+var current_number = 0
+var my_random_number = 1
+var current_flip = false
+
+func _ready():
+	_timer = Timer.new()
+	add_child(_timer)
+	_timer.connect("timeout", self, "_on_Timer_timeout")
+	_timer.set_wait_time(4.0)
+	_timer.set_one_shot(false) # Make sure it loops
+	_timer.start()
+	
+func _physics_process(delta):
+	var gerakan = Vector2(arah * speed, 0)
+	gerakan.y = my_random_number*7
+	if(my_random_number > 0):
+		gerakan.x = 100
+		arah = 1
+		$monster_1.flip_h = true
+	if(my_random_number < 0):
+		gerakan.x = -100
+		arah = -1
+		$monster_1.flip_h = false
+	
+	if ($monster_1_atas.is_colliding() or $monster_1_bawah.is_colliding()):
+		arah = arah*-1
+	if ($monster_1_kanan.is_colliding()):
+		arah = arah*-1
+		$monster_1.flip_h = true
+	if ($monster_1_kiri.is_colliding()):
+		arah = arah*-1
+		$monster_1.flip_h = false
+	move_and_slide(gerakan)
+		
+
+func _on_Timer_timeout():
+	rng.randomize()
+	my_random_number = rng.randf_range(-17, 17)
+	print(my_random_number)
+
